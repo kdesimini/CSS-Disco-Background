b4f5c91dc620150149f7455ae4424d369b8a3e5d
Keith <34429274+kdesimini@users.noreply.github.com>
2019-10-12 18:56:57 -0400

    Remove more dumb colors and enable file generation

--- spaz.py
+++ spaz.py
@@ -1,26 +1,53 @@
 import random
 
-colors = ["Aqua ","Aquamarine ","Bisque ","BlanchedAlmond ","Blue ","BlueViolet ","Chartreuse ","Coral ","CornflowerBlue ","Crimson ","Cyan ","DarkBlue ","DarkCyan ","DarkGoldenRod ","DarkMagenta ","DarkOliveGreen ","DarkOrange ","DarkOrchid ","DarkRed ","DarkSlateBlue ","DarkSlateGray ","DarkSlateGrey ","DarkTurquoise ","DarkViolet ","DeepPink ","DeepSkyBlue ","DodgerBlue ","FireBrick ","ForestGreen ","Fuchsia ","Gold ","GoldenRod ","Green ","GreenYellow ","HotPink ","IndianRed  ","Indigo  ","LawnGreen ","LightBlue ","LightCoral ","LightGreen ","LightPink ","LightSalmon ","LightSeaGreen ","LightSkyBlue ","Lime ","LimeGreen ","Magenta ","Maroon ","MediumAquaMarine ","MediumBlue ","MediumOrchid ","MediumPurple ","MediumSeaGreen ","MediumSlateBlue ","MediumSpringGreen ","MediumTurquoise ","MediumVioletRed ","MidnightBlue ","Navy ","Olive ","OliveDrab ","Orange ","OrangeRed ","Orchid ","PaleGreen ","PaleTurquoise ","PaleVioletRed ","Peru ","Pink ","Plum ","PowderBlue ","Purple ","RebeccaPurple ","Red ","RosyBrown ","RoyalBlue ","SaddleBrown ","Salmon ","SandyBrown ","SeaGreen ","SeaShell ","Sienna ","Silver ","SkyBlue ","SlateBlue ","SlateGray ","SlateGrey ","Snow ","SpringGreen ","SteelBlue ","Tan ","Teal ","Thistle ","Tomato ","Turquoise ","Violet ","Wheat ","White ","WhiteSmoke ","Yellow ","YellowGreen "]
+colors = ["Aqua","Aquamarine","Bisque","BlanchedAlmond","Blue","BlueViolet","Chartreuse","Coral","CornflowerBlue","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkMagenta","DarkOliveGreen","DarkOrange","DarkOrchid","DarkRed","DarkSlateBlue","DarkTurquoise","DarkViolet","DeepPink","DodgerBlue","FireBrick","ForestGreen","Fuchsia","Gold","GoldenRod","Green","GreenYellow","HotPink","IndianRed","Indigo","LawnGreen","LightBlue","LightCoral","LightGreen","LightSeaGreen","Lime","LimeGreen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","Navy","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGreen","PaleTurquoise","PaleVioletRed","Peru","Pink","Plum","PowderBlue","Purple","RebeccaPurple","Red","RosyBrown","RoyalBlue","SeaGreen","Sienna","SlateBlue","SlateGray","SpringGreen","SteelBlue","Teal","Thistle","Tomato","Turquoise","Violet","Yellow","YellowGreen"]
+
+# Parameters
+maxWidth = 1920 # in pixles
+minWidth = 280 # in pixles
+density = 20 # pixles before new color is generated
+useTransition = False
+useDelay = False # prevents color change until resizing stops
+transitionString = "transition: background 0.1s linear; "
+delayString = "transition-delay: .2s;"
+saveToFile = True # make this false if you just want console output. 
+fileName = "discojam.css"
+reset = True
+
+
+def outputToConsole(maxWidth):
+    print("@media (max-width: " + str(maxWidth) + "px) and (max-height: " + str(maxWidth) + "px) {")
+    print("    body {")
+    if useTransition : print("        " + transitionString)
+    if useDelay : print ("        " + delayString)
+    print("        background: " + str(random.choice(colors)) +";")
+    print("    }\n}")
+
+    
+def outputToFile(maxWidth):
+    global reset
+    if reset == True:
+        reset = False
+        open(fileName, 'w').close() # reset the file to clean slate
+    file = open(fileName, "a+")
+    file.write("@media (max-width: " + str(maxWidth) + "px) and (max-height: " + str(maxWidth) + "px) {\n")
+    file.write("    body {\n")
+    if useTransition : file.write("        " + transitionString + "\n")
+    if useDelay : file.write ("        " + delayString + "\n")
+    file.write("        background: " + str(random.choice(colors)) +";\n")
+    file.write("    }\n}")
+    file.close()
+
 def generate():
-
-    # Parameters
-    maxWidth = 1920 # in pixles
-    minWidth = 280 # in pixles
-    density = 10 # pixles before new color is generated
-    useTransition = True
-    useDelay = False # prevents color change until resizing stops
-    transitionString = "transition: background 0.7s linear; "
-    delayString = "transition-delay: .2s;"
-
     # The obsurdity
+    global maxWidth
     while maxWidth >= minWidth:
-        print("@media (max-width: " + str(maxWidth) + "px) and (max-height: " + str(maxWidth) + "px) {")
-        print("    body {")
-        if useTransition : print("        " + transitionString)
-        if useDelay : print ("        " + delayString)
-        print("        background: " + str(random.choice(colors)) +";")
-        print("    }\n}")
+        if saveToFile == True:
+            outputToFile(maxWidth)
+        else:
+            outputToConsole(maxWidth)
         maxWidth -= density
+
 
 if __name__ == "__main__":
     generate()
